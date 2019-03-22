84ecfa27628095200049ee9b05b3ca672ba8d4a3
skoba <skoba@man.poznan.pl>
2019-03-22 12:24:56 +0100

    Initial Commit

---
+++ lib/home/home_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:deep_app/utils/constants.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primary_color,
+        title: Text(AppStrings.app_label),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+          // sets the background color of the `BottomNavigationBar`
+          canvasColor: AppColors.primary_color,
+          // sets the active color of the `BottomNavigationBar` if `Brightness` is light
+          primaryColor: Colors.white,
+          textTheme: Theme
+            .of(context)
+            .textTheme
+            .copyWith(caption: new TextStyle(color: Colors.white70))),
+        child: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex, // this will be set when a new tab is tapped
+          items: [
+              BottomNavigationBarItem(
+            icon: new Icon(Icons.add_circle_outline),
+            title: new Text(AppStrings.analysis),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.access_time),
+            title: new Text(AppStrings.history),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline),
+              title: Text(AppStrings.credits)
+          )
+        ],
+      ),
+    )
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppStrings {
+  static String app_label = "Marine Conus API";
+  static String analysis = "Analysis";
+  static String history = "History";
+  static String credits = "Credits";
+}
+
+class AppColors {
+  static const Color primary_color = Color(0xFF35B4C5);
+}
