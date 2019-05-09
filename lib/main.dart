9f3ea22215ead06f85cd6c07048cf96400565d4d
Sebastian Koba <skoba@man.poznan.pl>
2019-05-09 10:18:22 +0200

    portrait orientation lock and dependencies update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'app/recognition_app.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'My Flutter App',
       home: RecognitionApp(),
