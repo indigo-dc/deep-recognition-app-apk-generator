1d35040c76522736d9559a6a8cac0ba3fe2c04dc
Sebastian Koba <skoba@man.poznan.pl>
2019-08-07 10:48:15 +0200

    mocking api response from resources

--- lib/api/mock_recognition_api.dart
+++ lib/api/mock_recognition_api.dart
@@ -1,9 +1,7 @@
-import 'package:deep_app/utils/constants.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 
 class MockRecognitionApi{
-  final String server = AppStrings.api_url;
 
   Future<String> postTask(List<String> photoPaths) async {
     final response = loadAsset();
